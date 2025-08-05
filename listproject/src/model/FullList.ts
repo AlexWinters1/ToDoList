71834bc872e91237a497c128ae59aabf0b008c0b
Alex Winters <alexandwinters@gmail.com>
2025-08-05 14:38:45 -0500

    Challenge2 FullList

---
+++ listproject/src/model/FullList.ts
@@ -0,0 +1,55 @@
+import ListItem from './ListItem'
+
+interface List {
+    //not really 
+    list: ListItem[],
+    load(): void,
+    save(): void,
+    clearList(): void,
+    addItem(itemObj: ListItem): void,
+    removeItem(id: string): void,
+}
+export default class FullList implements List {
+    //instantiating a new list
+    static instance: FullList = new FullList()
+    //receives private list
+    private constructor(
+        private _list: ListItem[] = [],
+    ) { }
+    //getter method for list
+    get list(): ListItem[] {
+        return this._list
+    }
+    //loading 
+    load(): void{
+        //defining stored list, union type 
+        const storedList: string | null = localStorage.getItem("myList")
+        //type guard
+        if (typeof storedList !== "string") return 
+        //define parsed list, needs its own type
+        const parsedList: { _id: string, _item: string, _checked: boolean}[] = JSON.parse(storedList)
+        parsedList.forEach(itemObj => {
+            const newListItem = new ListItem(itemObj._id, itemObj._item,itemObj._checked)
+            FullList.instance.addItem(newListItem)
+        })
+    }
+    //saving
+    save(): void {
+        localStorage.setItem("myList", JSON.stringify(this._list))
+    }
+    //clearing
+    clearList(): void{
+        this._list = [];
+        this.save()
+    }
+    //adding
+    addItem(itemObj: ListItem): void {
+        this._list.push(itemObj)
+        this.save()
+    }
+    //removing
+    removeItem(id: string): void {
+        this._list= this._list.filter(item => item.id !== id)
+        this.save()
+    }
+}
