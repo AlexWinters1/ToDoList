8f8e3e9862c88e4c97f156e2bdcd88710fd310af
Alex Winters <alexandwinters@gmail.com>
2025-08-08 18:04:05 -0500

    planning the structure

--- listproject/src/main.ts
+++ listproject/src/main.ts
@@ -1,1 +1,22 @@
 import './css/style.css'
+import FullList from './model/FullList'
+import ListItem from './model/ListItem'
+import ListTemplate from './templates/ListTemplate'
+
+//initialize app func
+    //get instances
+    //fullList
+    //template
+//itemEntryForm get by id type assert as HTMLFormEle
+    //add event listener function
+        //prevent default
+    //create handler
+    // const input and get elem by id making it "newItem"
+    //const newEntryText as a string then 
+//clear Items const type assert, add listener method to clear items button 
+    //fullList.clearItems 
+    //template.clear()
+//call load 
+//call template render and pass in fullList
+
+//add event listener to doc itself and call DOMContentLoaded
